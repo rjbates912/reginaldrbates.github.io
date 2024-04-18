a4ef92f9b53cfaaf2a3ef802ea7493975efff667
Reginald Bates <rjbates912@gmail.com>
2024-04-18 14:27:27 +0000

    describe your changes

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -18,12 +18,350 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
-
     
+    function createSawBlade(x, y){
+      var hitZoneSize = 25;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x = -25;
+      obstacleImage.y = -25;
+      sawBladeHitZone.rotationalVelocity = 10;
+    }
+    
+    function createSpikeTrap(x, y){
+      var hitZoneSize = 25;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
+      var obstacleImage = draw.bitmap("img/SpikeTrap.png");
+      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x = -20;
+      obstacleImage.y = -30;
+      obstacleImage.scaleX = 0.5;
+      obstacleImage.scaleY = 0.5;
+    }
+    
+    function createSpikeTrap2(x, y){
+      var hitZoneSize = 25;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
+      var obstacleImage = draw.bitmap("img/SpikeTrap.png");
+      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x = -20;
+      obstacleImage.y = -30;
+      obstacleImage.scaleX = 0.5;
+      obstacleImage.scaleY = 0.5;
+    }
 
+    function createEnemy(x, y){
+      var enemy = game.createGameItem("enemy", 25); // causes damage to Hallebot
+      var redSquare = draw.bitmap("img/Goomba (1).png");
+      redSquare.x = -25;
+      redSquare.y = -25;
+      enemy.addChild(redSquare);
+      enemy.x = x;
+      enemy.y = y;
+      game.addGameItem(enemy);
+      enemy.velocityX = -4;
+      enemy.rotationalVelocity = 0;
+      redSquare.scaleX = 0.3;
+      redSquare.scaleY = 0.3;
+      enemy.onPlayerCollision = function(){
+        game.changeIntegrity(-10)
+      };
+
+      enemy.onProjectileCollision = function(){
+        game.increaseScore(2000);
+        enemy.fadeOut();
+        // enemy.shrink()
+        // enemy.flyTo(0, 0)
+    }
+ }
+
+ function createEnemy2(x, y){
+  var enemy = game.createGameItem("enemy", 25);
+  var redSquare = draw.bitmap("img/Spike.png");
+  redSquare.x = -25;
+  redSquare.y = -25;
+  enemy.addChild(redSquare);
+  enemy.x = x;
+  enemy.y = y;
+  game.addGameItem(enemy);
+  enemy.velocityX = -4;
+  enemy.rotationalVelocity = 0;
+  redSquare.scaleX = 0.3;
+  redSquare.scaleY = 0.3;
+  enemy.onPlayerCollision = function(){
+    game.changeIntegrity(-10)
+  };
+
+  enemy.onProjectileCollision = function(){
+    game.increaseScore(2000);
+    enemy.fadeOut();
+    // enemy.shrink()
+    // enemy.flyTo(0, 0)
+  }
+}
+
+  function createEnemy3(x, y){
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.bitmap("img/KoopaTroopa (1).png");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = x;
+    enemy.y = y;
+    game.addGameItem(enemy);
+    enemy.velocityX = -4;
+    enemy.rotationalVelocity = 0;
+    redSquare.scaleX = 0.3;
+    redSquare.scaleY = 0.3;
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10)
+    };
+    
+    enemy.onProjectileCollision = function(){
+      game.increaseScore(2000);
+      enemy.fadeOut();
+      // enemy.shrink()
+      // enemy.flyTo(0, 0)
+    }
+    
+  }
+
+  function createEnemy4(x, y){
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.bitmap("img/BulletBill.png");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = x;
+    enemy.y = y;
+    game.addGameItem(enemy);
+    enemy.velocityX = -4;
+    enemy.rotationalVelocity = 0;
+    redSquare.scaleX = 0.3;
+    redSquare.scaleY = 0.3;
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10)
+    };
+    
+    enemy.onProjectileCollision = function(){
+      game.increaseScore(2000);
+      enemy.fadeOut();
+      // enemy.shrink()
+      // enemy.flyTo(0, 0)
+    }
+    
+  }
+
+  function createEnemy5(x, y){
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.bitmap("img/Goomba (1).png");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = x;
+    enemy.y = y;
+    game.addGameItem(enemy);
+    enemy.velocityX = -4;
+    enemy.rotationalVelocity = 0;
+    redSquare.scaleX = 0.3;
+    redSquare.scaleY = 0.3;
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10)
+    };
+    
+    enemy.onProjectileCollision = function(){
+      game.increaseScore(2000);
+      enemy.fadeOut();
+      // enemy.shrink()
+      // enemy.flyTo(0, 0)
+    }
+    
+  }
+
+  function createEnemy6(x, y){
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.bitmap("img/Spike.png");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = x;
+    enemy.y = y;
+    game.addGameItem(enemy);
+    enemy.velocityX = -4;
+    enemy.rotationalVelocity = 0;
+    redSquare.scaleX = 0.3;
+    redSquare.scaleY = 0.3;
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10)
+    };
+    
+    enemy.onProjectileCollision = function(){
+      game.increaseScore(2000);
+      enemy.fadeOut();
+      // enemy.shrink()
+      // enemy.flyTo(0, 0)
+    }
+    
+  }
+
+  function createEnemy7(x, y){
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.bitmap("img/KoopaTroopa (1).png");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = x;
+    enemy.y = y;
+    game.addGameItem(enemy);
+    enemy.velocityX = -4;
+    enemy.rotationalVelocity = 0;
+    redSquare.scaleX = 0.3;
+    redSquare.scaleY = 0.3;
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10)
+    };
+    
+    enemy.onProjectileCollision = function(){
+      game.increaseScore(2000);
+      enemy.fadeOut();
+      // enemy.shrink()
+      // enemy.flyTo(0, 0)
+    }
+    
+  }
+
+  function createEnemy8(x, y){
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.bitmap("img/BulletBill.png");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = x;
+    enemy.y = y;
+    game.addGameItem(enemy);
+    enemy.velocityX = -4;
+    enemy.rotationalVelocity = 0;
+    redSquare.scaleX = 0.3;
+    redSquare.scaleY = 0.3;
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10)
+    };
+    
+    enemy.onProjectileCollision = function(){
+      game.increaseScore(2000);
+      enemy.fadeOut();
+      // enemy.shrink()
+      // enemy.flyTo(0, 0)
+    }
+    
+  }
+    function createReward(x, y){
+      var reward = game.createGameItem("enemy", 25); // gains points and health
+      var redSquare = draw.bitmap("img/PowerUp.png");
+      redSquare.x = -45;
+      redSquare.y = -45;
+      reward.addChild(redSquare);
+      reward.x = x;
+      reward.y = y;
+      game.addGameItem(reward);
+      reward.velocityX = -4;
+      reward.rotationalVelocity = 0 ;
+      redSquare.scaleX = 0.1;
+      redSquare.scaleY = 0.1;
+      reward.onPlayerCollision = function(){
+        game.changeIntegrity(+10)
+        reward.fadeOut();
+        game.increaseScore(2000);
+      };
+
+      reward.onProjectileCollision = function(){
+        game.increaseScore(2000);
+        reward.fadeOut();
+        // reward.shrink()
+        // reward.flyTo(0, 0)
+    }
+ }
+
+ function createMarker(x, y){
+  var marker = game.createGameItem("marker", 25); // end of the level
+  var blueSquare = draw.bitmap("img/Pipe.png");
+  blueSquare.x = -40;
+  blueSquare.y = -40;
+  marker.addChild(blueSquare);
+  marker.x = x;
+  marker.y = y;
+  game.addGameItem(marker);
+  marker.velocityX = -4;
+  marker.rotationalVelocity = 0;
+  blueSquare.scaleX = 0.5;
+  blueSquare.scaleY = 0.5;
+  marker.onPlayerCollision = function() {
+    game.changeIntegrity(+10)
+    startLevel();
+    marker.fadeOut();
+  };
+ }
+
+  // function calls
     function startLevel() {
       // TODO 13 goes below here
-
+      var level = levelData[currentLevel];
+      var levelObjects = level.gameItems;
+    for (var i = 0; i < levelObjects.length; i++){
+      var element = levelObjects[i];
+      if (element.type === "sawblade"){
+        createSawBlade(element.x, element.y);
+      }
+      if (element.type === "SpikeTrap"){
+        createSpikeTrap(element.x, element.y);
+      }
+      if (element.type === "SpikeTrap2"){
+        createSpikeTrap(element.x, element.y);
+      }
+      if (element.type === "enemy"){
+        createEnemy(element.x, element.y);
+      }
+      if (element.type === "enemy2"){
+        createEnemy2(element.x, element.y);
+      }
+      if (element.type === "enemy3"){
+        createEnemy3(element.x, element.y);
+      }
+      if (element.type === "enemy4"){
+        createEnemy4(element.x, element.y);
+      }
+      if (element.type === "enemy5"){
+        createEnemy(element.x, element.y);
+      }
+      if (element.type === "enemy6"){
+        createEnemy2(element.x, element.y);
+      }
+      if (element.type === "enemy7"){
+        createEnemy3(element.x, element.y);
+      }
+      if (element.type === "enemy8"){
+        createEnemy4(element.x, element.y);
+      }
+      if (element.type === "reward"){
+        createReward(element.x, element.y);
+      }
+      if (element.type === "marker"){
+        createMarker(element.x, element.y);
+      }
+    }
 
 
       //////////////////////////////////////////////
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -29,8 +29,10 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
-      
+        var tree;
+        var buildings = [];
+
+    
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -39,17 +41,48 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,groundY,'blue');
             background.addChild(backgroundFill);
             
+            var marioBG = draw.bitmap("img/Super Mario Background.jpg"); // image of the background
+            marioBG.x = 0;
+            marioBG.y = -350;
+            marioBG.scaleX = 1;
+            marioBG.scaleY = 0.85;
+            background.addChild(marioBG); // moon is part of the background
+
             // TODO 2: - Add a moon and starfield
             
+            for(var stars = 0; stars < 400; stars++){
+                var circle = draw.circle(3, "white", "white", 2); // size of the stars
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+            }
+            
+            var moon = draw.bitmap("img/moon.png"); // image of the moon
+            moon.x = canvasWidth - 500;
+            moon.y = groundY - 400;
+            moon.scaleX = 0.4;
+            moon.scaleY = 0.4;
+            background.addChild(moon); // moon is part of the background
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            var buildingColors = ["cyan", "yellow", "red", "purple", "green"];
+            for (var i = 0; i < 5; i++) {
+                var buildingHeight = 400 * Math.random();
+                var building = draw.rect(75, buildingHeight, buildingColors[i], "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
             
             // TODO 3: Part 1 - Add a tree
-            
+            tree = draw.bitmap("img/tree.png"); // image of a tree
+            tree.x = canvasWidth - 200; 
+            tree.y = groundY - 230;
+            background.addChild(tree); // tree is part of the background
             
         } // end of render function - DO NOT DELETE
         
@@ -63,10 +96,22 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 4; // speed of the tree
+
+            if (tree.x < -300) {
+            tree.x = canvasWidth; // stopping point and repeating cycle of the tree
+            }
             
             // TODO 4: Part 2 - Parallax
-            
+            for (var i = 0; i < buildings.length; i++) {
+                var building = buildings[i];
+                building.x = building.x - 1;
+
+                if(building.x < -100){
+                    building.x = canvasWidth;
+                }
+                // code to do something with each element
+              }
 
         } // end of update function - DO NOT DELETE
         
